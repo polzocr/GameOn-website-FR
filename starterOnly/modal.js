8f6aca448f6143f662feb56492902fdf88320714
Polzocr <micro.cpt.62@outlook.fr>
2022-10-19 10:07:18 +0200

    ajout close btn modal

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,13 +11,22 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+//ajout de l'élément du DOM close btn
+const closeBtn = document.querySelector('.close');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+//close modal event
+closeBtn.addEventListener('click', () => {
+  modalbg.style.display = 'none';
+})
 
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
 }
+
+
 
 
